45081b9997ddba9037bfdfcc75f4f6d71dda2fcf
Nicholas Rehac <tup88289@temple.edu>
2024-03-12 12:23:07 -0400

    fragment actiivty! :)

--- app/src/main/java/edu/temple/fragmentactivity1/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentactivity1/MainActivity.kt
@@ -2,10 +2,16 @@ package edu.temple.fragmentactivity1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentContainerView2 = findViewById<FragmentContainerView>(R.id.fragmentContainerView2)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView2, ColorFragment())
+            .commit()
+
     }
 }
